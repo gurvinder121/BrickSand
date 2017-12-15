e96a737e0ffed3ff2a6b6ae82ab0dfe667219b34
amarpreet <amarpreetsingh121@gmail.com>
2017-12-15 07:47:27 +0530

    first commit

---
+++ src/com/stone/Duplicate.java
@@ -0,0 +1,24 @@
+package com.stone;
+
+public class Duplicate {
+	public static void main(String args[])
+	{
+		
+	int[] arr=new int[5];
+	//int[] arr1=new int[5,12,34,56,34,34];
+	//String[] arrstr=["qwer","asd"];
+	arr[0]=7;
+	arr[1]=71;
+	arr[2]=76;
+	arr[3]=756;
+	arr[4]=2;
+	
+	System.out.println(arr[4]);
+	for(int i=0;i<=4;i++)
+	System.out.println(arr[i]);
+	
+	//for(j=0;j<=4;i++)
+	//System.out.println(i);
+	System.out.println(arr.length);
+		
+}}
